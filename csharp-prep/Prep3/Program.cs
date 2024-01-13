dd2f7fa63d82f79cf3fddcbb74976251a18b2df2
MarkTaylorTsai <marktaylortsai@gmail.com>
2024-01-13 16:14:31 +0800

    1.1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random numGenerator = new Random();
+        int number = numGenerator.Next(1, 99);
+        int answerInt = 0;
+        while (!(answerInt == number))
+        {
+            Console.Write("What is the magic number? ");
+            string answer = Console.ReadLine();
+            answerInt = int.Parse(answer);
+            if (number > answerInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = 0;
+        List<int> numbers = new List<int>();
+        while (true)
+        {
+            Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                break;
+            }
+            numbers.Add(number);
+        }
+        int sum = 0;
+        float ave = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+            ave = sum / numbers.Count;
+        };
+        int max = 0;
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {ave}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
