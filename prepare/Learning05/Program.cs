dfb758d8890fc392a199034425c52d47445db3f9
MarkTaylorTsai <marktaylortsai@example.com>
2024-02-11 17:27:33 +0800

    5-1

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 2.5),
+            new Rectangle("Blue", 4.0, 3.0),
+            new Circle("Green", 1.5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
